1d7930f9f40a075315e0314d6251f549ec0b7aa1
dodji05 <gildas31@gmail.com>
2018-07-27 18:10:25 +0100

    Initial commit

---
+++ src/AppBundle/Controller/OrderController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+class OrderController extends BaseController
+{
+    /**
+     * @Route("/cart/product/{slug}", name="order_add_product_to_cart")
+     * @Method("POST")
+     */
+    public function addProductToCartAction(Product $product)
+    {
+        $this->get('shopping_cart')
+            ->addProduct($product);
+
+        $this->addFlash('success', 'Product added!');
+
+        return $this->redirectToRoute('order_checkout');
+    }
+
+    /**
+     * @Route("/checkout", name="order_checkout")
+     * @Security("is_granted('ROLE_USER')")
+     */
+    public function checkoutAction(Request $request)
+    {
+        $products = $this->get('shopping_cart')->getProducts();
+
+        if ($request->isMethod('POST')) {
+            $token = $request->request->get('stripeToken');
+            \Stripe\Stripe::setApiKey("sk_test_ZkFGZwpPh4dleQVRYNWSOEAj");
+            \Stripe\Charge::create(array(
+                "amount" => $this->get('shopping_cart')->getTotal() * 100,
+                "currency" => "usd",
+                "source" => $token,
+                "description" => "First test charge!"
+            ));
+
+            $this->get('shopping_cart')->emptyCart();
+            $this->addFlash('success', 'Order Complete! Yay!');
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('order/checkout.html.twig', array(
+            'products' => $products,
+            'cart' => $this->get('shopping_cart')
+        ));
+
+    }
+}
